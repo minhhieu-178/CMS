aa2e865670be5b892cfd712edc00e088fb9ecd4a
minhhieu-178 <tahieu015@gmail.com>
2025-12-30 16:02:48 +0700

    fix2

---
+++ server/routes/educatorRoutes.js
@@ -0,0 +1,9 @@
+import express from 'express'
+import { updateRoleToEducator } from '../controllers/educatorController.js';
+
+const educatorRouter = express.Router()
+
+// Add educator role
+educatorRouter.get('/update-role', updateRoleToEducator)
+
+export default educatorRouter;
