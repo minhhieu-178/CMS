e7141059bbf63f1bd7f4605f772898d2fc827ac1
minhhieu-178 <tahieu015@gmail.com>
2025-12-30 16:32:04 +0700

    fix4

--- server/controllers/educatorController.js
+++ server/controllers/educatorController.js
@@ -1,27 +1,19 @@
-export const updateRoleToEducator = async (req, res) => {
+import { clerkClient } from '@clerk/express'
+
+// update role to educator
+export const updateRoleToEducator = async (req, res)=>{
     try {
         const userId = req.auth.userId
-        
-        if (!userId) {
-            return res.status(401).json({ 
-                success: false, 
-                message: 'Chưa xác thực' 
-            })
-        }
 
         await clerkClient.users.updateUserMetadata(userId, {
-            publicMetadata: {
+            publicMetadata:{
                 role: 'educator',
             }
         })
 
-        res.json({ success: true, message: 'Bạn đã có thể xuất bản khóa học' })
+        res.json({ success: true, message: 'You can publish a course now' })
 
     } catch (error) {
-        console.error('Lỗi khi cập nhật vai trò:', error)
-        res.status(500).json({ 
-            success: false, 
-            message: 'Không thể cập nhật vai trò' 
-        })
+        res.json({ success: false, message: error.message })
     }
 }
